cf87535734f6a63582a0eccf8f8a05a343cd2dc5
Steve Burns <steven.m.burns@intel.com>
2019-01-10 06:49:54 -0800

    Added command line arguments.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ To run at Intel:
 ````
 source ~/ppt_work_area/general/bin/activate.csh
 
-python gen_lp.py > foo.lp
+python gen_lp.py -n foo -ifn foo.rer
 
 /nfs/site/proj/dt/pdx_sde02/x86-64_linux26/cplex/12.2/cplex/bin/x86_sles10_4.1/cplex << EOF
 read foo.lp
--- gen_lp.py
+++ gen_lp.py
@@ -15,19 +15,27 @@ class NodeMgr:
 
         return self.nodes[n]
         
+import argparse
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser( description="Generate .lp file from text file with 4 fields per line: u, v, alpha, epsilon")
+
+    parser.add_argument( "-n", "--block_name", type=str, required=True)  
+    parser.add_argument( "-ifn", "--input_file_name", type=str, required=True)  
+
+    args = parser.parse_args()
+
     p = re.compile( "^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*$")
 
     # u v d e
     # t(u) + d - e p <= t(v)
-    # t(u) - t(v) -e p <= -d
-    # t(v) - t(u) +e p >=  d
-
+    # t(u) - t(v) - e p <= -d
+    # t(v) - t(u) + e p >=  d
 
     mgr = NodeMgr()
 
     lst = []
-    with open( "__ers", "rt") as fp:
+    with open( args.input_file_name, "rt") as fp:
         for line in fp:
             line = line.rstrip('\n')
             m = p.match( line)
@@ -39,10 +47,12 @@ if __name__ == "__main__":
             else:
                 assert False
 
-    print( "Maximize")
-    print( " obj: p")
-    print( "Subject To")
-    for (i,tup) in enumerate(lst):
-        print( " c%d: x%d - x%d + %d p >= %g" % (i,tup[1],tup[0],tup[3],tup[2]))
-    print( "Bounds")
-    print( " 0 <= p")
+    with open( args.block_name + ".lp", "wt") as fp:
+        print( "Minimize", file=fp)
+        print( " obj: p", file=fp)
+        print( "Subject To", file=fp)
+        for (i,tup) in enumerate(lst):
+            print( " c%d: x%d - x%d + %d p >= %g" % (i,tup[1],tup[0],tup[3],tup[2]), file=fp)
+        print( "Bounds", file=fp)
+        print( " 0 <= p", file=fp)
+        print( "End", file=fp)
